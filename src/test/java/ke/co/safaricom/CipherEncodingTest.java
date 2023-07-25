1fdb9c5daf3305118c5e40d8caaa8f9870040350
idah <idah@moringaproject.com>
2023-07-26 00:54:53 +0300

    created a CipherEncodingTest class

---
+++ src/test/java/ke/co/safaricom/CipherEncodingTest.java
@@ -0,0 +1,12 @@
+package ke.co.safaricom;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CipherEncodingTest {
+
+    @Test
+    void encode() {
+    }
+}
