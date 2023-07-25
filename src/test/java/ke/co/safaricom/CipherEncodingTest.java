1de93ff6a7884e50f9bd12d87063a8c43d83b220
idah <idah@moringaproject.com>
2023-07-26 00:59:10 +0300

    added a test for testing the functionality of encoding

--- src/test/java/ke/co/safaricom/CipherEncodingTest.java
+++ src/test/java/ke/co/safaricom/CipherEncodingTest.java
@@ -1,5 +1,6 @@
 package ke.co.safaricom;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,6 +8,15 @@ import static org.junit.jupiter.api.Assertions.*;
 class CipherEncodingTest {
 
     @Test
-    void encode() {
+    @DisplayName("Caesar Cipher Encoding a Single LowerCase Character and No Wrap Around")
+    void encodeSingleLowerCaseCharacterNoWrapAround() {
+        assertEquals("b", CipherEncoding.encode("a", 1));
+        assertEquals("e", CipherEncoding.encode("c", 2));
+        assertEquals("i", CipherEncoding.encode("b", 7));
+        assertEquals("z", CipherEncoding.encode("t", 6));
+        assertEquals("h", CipherEncoding.encode("e", 3));
+        assertEquals("y", CipherEncoding.encode("x", 1));
+        assertEquals("z", CipherEncoding.encode("y", 1));
+        assertEquals("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG", CipherEncoding.encode("QEB NRFZH YOLTK CLU GRJMP LSBO QEB IXWV ALD",3));
     }
 }
