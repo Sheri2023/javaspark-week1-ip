223027df1c7c7ce64b69596dc70e63044d2a8313
idah <idah@moringaproject.com>
2023-07-26 00:53:14 +0300

    created a cipherDecoding test class

--- src/main/java/ke/co/safaricom/CipherEncoding.java
+++ src/main/java/ke/co/safaricom/CipherEncoding.java
@@ -1,4 +1,27 @@
 package ke.co.safaricom;
 
 public class CipherEncoding {
+    private static String uppercaseAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static String lowercaseAlphabets = "abcdefghijklmnopqrstuvwxyz";
+    public static String encode(String msg, int key){
+        String output = "";
+        for (char c: msg.toCharArray()){
+            String alphabet=uppercaseAlphabets;
+            if (Character.isLowerCase(c)){
+                alphabet=lowercaseAlphabets;
+            }
+            int charPos = alphabet.indexOf(c);
+            if (charPos == -1){
+                output = output + c;
+                continue;
+            }
+            int tempNewPos = charPos + key;
+            int newPos = tempNewPos > 25? tempNewPos % 26 : tempNewPos;
+            char replacement = alphabet.charAt(newPos);
+            output = output + replacement;
+        }
+        return output;
+    }
+
 }
+
--- src/main/java/ke/co/safaricom/Main.java
+++ src/main/java/ke/co/safaricom/Main.java
@@ -1,7 +1,38 @@
 package ke.co.safaricom;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner userInputObject = new Scanner(System.in);
+
+        System.out.println("Select Process enter e for encode/d-decode!");
+        String process = userInputObject.nextLine();
+
+        System.out.println("Enter Message!");
+        String msg = userInputObject.nextLine();
+
+        System.out.println("Enter Key!");
+        int key = userInputObject.nextInt();
+
+        System.out.println("Processing...");
+        System.out.println(process);
+
+        if (process.toLowerCase().startsWith("e")){
+            String output = CipherEncoding.encode(msg, key);
+            System.out.println(output);
+
+        } else if (process.startsWith("d")){
+            String output = CipherDecoding.decode(msg, key);
+            System.out.println(output);
+        }
+        else {
+            System.out.println("invalid process");
+
+        }
+
+
+
+
     }
 }
